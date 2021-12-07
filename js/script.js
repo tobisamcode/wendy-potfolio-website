b3c2e60b705600f83117b12cf73e1bb258794074
tobisamcode <samueloluwatobiloba48@gmail.com>
2021-12-07 19:39:34 +0100

    changes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 /*==================== CHANGE BACKGROUND HEADER ====================*/
 function scrollHeader() {
     const nav = document.getElementById('header')
-        // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
-    if (this.scrollY >= 50) nav.classList.add('scroll-header');
+        // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
+    if (this.scrollY >= 100) nav.classList.add('scroll-header');
     else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
